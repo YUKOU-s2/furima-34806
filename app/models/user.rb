4043ae8fdf9c5bd7a60d356a9d4b1058eeb10346
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-03-30 15:15:40 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+       with_options presence: true do
          ZENKAKU_REGEXP  = /\A[\p[ぁ-んァ-ヶ一-龥々]\u{30fc}]+\z/
-         validates :first_name, presence: true, format: { with: ZENKAKU_REGEXP }
-         validates :family_name, presence: true, format: { with: ZENKAKU_REGEXP }
-         KATAKANA_REGEXP  = /\A[\p[ァ-ヶー－]\u{30fc}]+\z/
-         validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
-         validates :family_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
-         validates :nickname, presence: true
-         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-         validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+         validates :first_name, format: { with: ZENKAKU_REGEXP }
+         validates :family_name, format: { with: ZENKAKU_REGEXP }
+         KATAKANA_REGEXP  = /\A[ァ-ヶー－]+\z/
+         validates :first_name_kana, format: { with: KATAKANA_REGEXP }
+         validates :family_name_kana, format: { with: KATAKANA_REGEXP }
+         validates :nickname
+         validates :dob
+       end
          VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
          validates :password,
             format: { with: VALID_PASSWORD_REGEX,
              message: "は半角6以上英文字・数字それぞれ１文字以上含む必要があります"}
-         validates :dob, presence: true
 
 end
