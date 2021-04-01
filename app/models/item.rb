b4fb34cab3548231605319330ba179b643b48547
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-04-01 20:21:23 +0900

    商品出品機能（修正）

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,13 @@ class Item < ApplicationRecord
   belongs_to :deliveryTime
   belongs_to :area
 
-  PRICE_REGEX =/[\d]/
+ 
   with_options presence: true do
   validates :item_name
   validates :introduction
   validates :image
   validates :price ,
-   numericality:{greater_than: 299, less_than: 10000000},
-     format: { with: PRICE_REGEX}
+   numericality:{greater_than: 299, less_than: 10000000}
   end
 
   with_options presence: true, numericality: { other_than: 1 }  do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,25 +25,50 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include "Category can't be blank"
     end
+    it 'category_idが1では登録できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Category must be other than 1"
+    end
     it 'condition_idが空では登録できない' do
       @item.condition_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include "Condition can't be blank"
+    end
+    it 'condition_idが1では登録できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Condition must be other than 1"
     end
     it 'charge_idが空では登録できない' do
       @item.charge_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include "Charge can't be blank"
     end
+    it 'charge_idが1では登録できない' do
+      @item.charge_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Charge must be other than 1"
+    end
     it 'area_idが空では登録できない' do
       @item.area_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include "Area can't be blank"
     end
+    it 'area_idが1では登録できない' do
+      @item.area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Area must be other than 1"
+    end
     it 'delivery_time_idが空では登録できない' do
       @item.delivery_time_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include "Delivery time can't be blank"
+    end
+    it 'delivery_time_idが1では登録できない' do
+      @item.delivery_time_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Delivery time must be other than 1"
     end
     it 'imageが空では登録できない' do
       @item.image = nil
@@ -65,8 +90,19 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include "Price must be less than 10000000"
     end
-    it 'priceが半角数字以外は登録できない' do
+    it 'priceが半角英語は登録できない' do
       @item.price = 'AAAA'
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price is not a number"
+    end
+    it 'priceが全角文字は登録できない' do
+      @item.price = 'あああ'
+      @item.valid?
+      binding.pry
+      expect(@item.errors.full_messages).to include "Price is not a number"
+    end
+    it 'priceが半角英数字混合は登録できない' do
+      @item.price = '11AA'
       @item.valid?
       expect(@item.errors.full_messages).to include "Price is not a number"
     end
