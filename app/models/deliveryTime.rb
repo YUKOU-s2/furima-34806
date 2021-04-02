9fae1924cd312fb82d6ed71cccc6f3d011605902
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-04-02 12:45:04 +0900

    商品一覧表示修正＋

--- app/models/deliveryTime.rb
+++ app/models/deliveryTime.rb
@@ -1,9 +1,9 @@
 class DeliveryTime < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '１日〜２日で発想' },
-    { id: 3, name: '3日〜４日で発想' },
-    { id: 4, name: '４日〜７日で発想' },
+    { id: 2, name: '１日〜２日で発送' },
+    { id: 3, name: '3日〜４日で発送' },
+    { id: 4, name: '４日〜７日で発送' },
   ]
     include ActiveHash::Associations
     has_many :items
