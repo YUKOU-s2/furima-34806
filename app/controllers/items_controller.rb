01016dec519d4f590bec84c19514b6385bfe72e4
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-04-02 16:49:23 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:show]
 
   def index
     @items = Item.order(created_at:"desc")
@@ -18,6 +18,21 @@ class ItemsController < ApplicationController
      end 
    end
 
+   def show
+    @item = Item.find(params[:id])
+   end
+
+   def edit
+   end
+   
+   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render root_path
+    end 
+   end
+
+
    private
     def item_params
          params.require(:item).permit(:item_name, :introduction,:category_id,:condition_id,:charge_id,:delivery_time_id,:area_id,:price,:image).merge(user_id: current_user.id)
@@ -25,8 +40,6 @@ class ItemsController < ApplicationController
  end
  
 
-  #def destroy
-  #end
 
 
 
